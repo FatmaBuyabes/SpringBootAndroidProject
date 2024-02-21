df16b79edd387ccb8179391df7aab674fba2f35f
FatmaBuyabes <buyabesfatemah@gmail.com>
2024-02-21 15:44:53 +0300

    still working

---
+++ app/src/main/java/com/joincoded/duolingoarabic/composable/component/PictureQuestion.kt
@@ -0,0 +1,70 @@
+package com.joincoded.duolingoarabic.composable.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.joincoded.duolingoarabic.R
+import com.joincoded.duolingoarabic.viewModel.GameViewModel
+
+
+@Composable
+fun PictureQuestion(viewModel: GameViewModel){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .paint(
+                painterResource(id = R.drawable.backround_question_image),
+                contentScale = ContentScale.FillBounds
+            ),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.a),
+            contentDescription = "Image",
+            modifier = Modifier
+        )
+        Spacer(modifier = Modifier.height(36.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.a),
+                contentDescription = "Image",
+                modifier = Modifier.clickable {/* img */ }
+            )
+            Spacer(modifier = Modifier.height(36.dp))
+
+            Image(
+                painter = painterResource(id = R.drawable.a),
+                contentDescription = "Image",
+                modifier = Modifier.clickable {/* img */ }
+            )
+        }
+
+        Spacer(modifier = Modifier.height(45.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.a),
+            contentDescription = "Image",
+            modifier = Modifier.clickable {/* img */ }
+
+        )
+
+    }
+}
